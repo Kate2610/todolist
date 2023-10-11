4e6aae41583c0dbb137d4ac550b863f53143f412
Kate2610 <yekaterina.anisimova@gmail.com>
2023-10-11 19:23:00 +0300

    filter added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,61 +1,68 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import EditTask from '../modals/EditTask'
 
-const Card = ({taskObj, index, deleteTask, updateListArray}) => {
+const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
     const [modal, setModal] = useState(false);
+    const [isDone, setIsDone] = useState(taskObj.Done || false);
 
     const colors = [
         {
-            primaryColor : "#5D93E1",
-            secondaryColor : "#ECF3FC"
+            primaryColor: "#5D93E1",
+            secondaryColor: "#ECF3FC"
         },
         {
-            primaryColor : "#F9D288",
-            secondaryColor : "#FEFAF1"
+            primaryColor: "#F9D288",
+            secondaryColor: "#FEFAF1"
         },
         {
-            primaryColor : "#5DC250",
-            secondaryColor : "#F2FAF1"
+            primaryColor: "#5DC250",
+            secondaryColor: "#F2FAF1"
         },
         {
-            primaryColor : "#F48687",
-            secondaryColor : "#FDF1F1"
+            primaryColor: "#F48687",
+            secondaryColor: "#FDF1F1"
         },
         {
-            primaryColor : "#B964F7",
-            secondaryColor : "#F3F0FD"
+            primaryColor: "#B964F7",
+            secondaryColor: "#F3F0FD"
         }
-    ]
+    ];
 
     const toggle = () => {
         setModal(!modal);
     }
 
-    const updateTask = (obj) => {
-        updateListArray(obj, index)
+    const updateTask = (updatedTask) => {
+        updateListArray(updatedTask, index);
+        toggle(); // Закрыть модальное окно после обновления
     }
 
     const handleDelete = () => {
-        deleteTask(index)
+        deleteTask(index);
+    }
+
+    const handleCheckboxChange = () => {
+        const updatedDone = !isDone;
+        setIsDone(updatedDone);
+        const updatedTask = { ...taskObj, Done: updatedDone };
+        updateListArray(updatedTask, index);
     }
 
     return (
-        <div class = "card-wrapper mr-5">
-            <div class = "card-top" style={{"background-color": colors[index%5].primaryColor}}></div>
-            <div class = "task-holder">
-                <span class = "card-header" style={{"background-color": colors[index%5].secondaryColor, "border-radius": "10px"}}>{taskObj.Name}</span>
-                <p className = "mt-3">{taskObj.Description}</p>
-                  <input type="checkbox" style={{ "position": "absolute", "left": "20px", "bottom": "25px" }} />
+        <div className="card-wrapper mr-5">
+            <div className="card-top" style={{ "background-color": colors[index % 5].primaryColor }}></div>
+            <div className="task-holder">
+                <span className="card-header" style={{ "background-color": colors[index % 5].secondaryColor, "borderRadius": "10px" }}>{taskObj.Name}</span>
+                <p className="mt-3">{taskObj.Description}</p>
+                <input type="checkbox" style={{ "position": "absolute", "left": "20px", "bottom": "25px" }} checked={isDone} onChange={handleCheckboxChange} />
                 <p style={{ "position": "absolute", "left": "40px", "bottom": "5px" }}>Done</p>
-                
-                <div style={{"position": "absolute", "right" : "20px", "bottom" : "20px"}}>
-                   
-                    <i class='far fa-edit' style={{ "color": colors[index % 5].primaryColor, "cursor": "pointer", marginRight: '10px' }} onClick={() => setModal(true)}></i>
-                    <i class="fas fa-trash-alt" style = {{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {handleDelete}></i>
-                </div>
 
-        </div>
-         <EditTask modal = {modal} toggle = {toggle} updateTask = {updateTask} taskObj = {taskObj}/>
+                <div style={{ "position": "absolute", "right": "20px", "bottom": "20px" }}>
+                    <i className='far fa-edit' style={{ "color": colors[index % 5].primaryColor, "cursor": "pointer", marginRight: '10px' }} onClick={() => setModal(true)}></i>
+                    <i className="fas fa-trash-alt" style={{ "color": colors[index % 5].primaryColor, "cursor": "pointer" }} onClick={handleDelete}></i>
+                </div>
+            </div>
+            <EditTask modal={modal} toggle={toggle} updateTask={updateTask} taskObj={taskObj} />
         </div>
     );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,54 +5,81 @@ import Card from './Card';
 const TodoList = () => {
     const [modal, setModal] = useState(false);
     const [taskList, setTaskList] = useState([]);
+    const [filterStatus, setFilterStatus] = useState('all');
 
-   useEffect(() => {
-    let arr = localStorage.getItem('taskList');
-    
-    if (arr) {
-        let obj = JSON.parse(arr);
-        setTaskList(obj);
-    }
-   }, []);
-    
+    useEffect(() => {
+        let arr = localStorage.getItem('taskList');
+
+        if (arr) {
+            let obj = JSON.parse(arr);
+            setTaskList(obj);
+        }
+    }, []);
+
     const deleteTask = (index) => {
-        let tempList = taskList
-        tempList.splice(index, 1)
-        localStorage.setItem('taskList', JSON.stringify(tempList))
-        setTaskList(tempList)
-        window.location.reload()
+        let tempList = [...taskList];
+        tempList.splice(index, 1);
+        localStorage.setItem('taskList', JSON.stringify(tempList));
+        setTaskList(tempList);
     }
 
     const updateListArray = (obj, index) => {
-        let tempList = taskList
-        tempList[index] = obj
-        localStorage.setItem('taskList', JSON.stringify(tempList))
-        setTaskList(tempList)
-        window.location.reload()
+        let tempList = [...taskList];
+        tempList[index] = obj;
+        localStorage.setItem('taskList', JSON.stringify(tempList));
+        setTaskList(tempList);
     }
 
-    
     const toggle = () => {
         setModal(!modal);
     }
 
     const saveTask = (taskObj) => {
-        let tempList = taskList
-        tempList.push(taskObj)
-        localStorage.setItem('taskList', JSON.stringify(tempList))
-        setTaskList(tempList)
-        setModal(false)
+        let tempList = [...taskList];
+        tempList.push(taskObj);
+        localStorage.setItem('taskList', JSON.stringify(tempList));
+        setTaskList(tempList);
+        setModal(false);
+    }
+
+    const handleFilterChange = (status) => {
+        setFilterStatus(status);
     }
 
     return (
         <>
-            <div className='header text-center'>
-                <h3>Todo List</h3> 
+            <div className='header text-center mb-10'>
+                <h3>Todo List</h3>
                 <button className='btn btn-primary mt-2' onClick={() => setModal(true)}>Create Task</button>
+
+                <div className='filter-buttons'>
+                    <button className='btn btn-secondary' onClick={() => handleFilterChange('all')}>All</button>
+                    <button className='btn btn-success' onClick={() => handleFilterChange('done')}>Done</button>
+                    <button className='btn btn-danger' onClick={() => handleFilterChange('to do')}>To do</button>
+                </div>
             </div>
 
             <div className='task-container'>
-                {taskList && taskList.map((obj, index) => <Card taskObj={obj} index={index} deleteTask={deleteTask} updateListArray={updateListArray} />)}
+                {taskList &&
+                    taskList.map((obj, index) => {
+                        const isCompleted = obj.Done; // Проверка на Done
+                        if (
+                            (filterStatus === 'done' && isCompleted) ||
+                            (filterStatus === 'to do' && !isCompleted) ||
+                            filterStatus === 'all'
+                        ) {
+                            return (
+                                <Card
+                                    taskObj={obj}
+                                    index={index}
+                                    deleteTask={deleteTask}
+                                    updateListArray={updateListArray}
+                                />
+                            );
+                        }
+                        return null;
+                    })
+                }
             </div>
 
             <CreateTask toggle={toggle} modal={modal} save={saveTask} />
