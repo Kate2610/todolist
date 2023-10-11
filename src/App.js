6152f90621b85326f0df220d537a064db61b40b0
Kate2610 <yekaterina.anisimova@gmail.com>
2023-10-11 13:27:40 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TodoList from './components/TodoList';
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+     
+    </div>
+  );
+}
+
+export default App;
